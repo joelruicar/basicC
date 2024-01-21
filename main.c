e81efaf458cc63811c34fd2fcf870c70ff03375a
Joel <79532707+joelruicar@users.noreply.github.com>
2024-01-22 00:34:24 +0100

    main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  // Definir las variables
+  int numero1 = 2;
+  int numero2 = 2;
+
+  // Realizar la suma
+  int resultado = numero1 + numero2;
+
+  // Imprimir el resultado en pantalla
+  printf("El resultado de la suma 2 + 2 es: %d\n", resultado);
+
+  return 0;
+}
